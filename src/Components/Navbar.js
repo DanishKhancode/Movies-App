2afdf62ece8463a8ce589971469c8da7cd2ce768
Danish <khandanish36408@gmail.com>
2024-01-14 00:59:22 +0530

    :rocket:Movies

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class 
+ extends Component {
+  render() {
+    return (
+      <div style={{display:'flex',backgroundColor:"lightcyan",justifyItems:"center"}}>
+        <h1 style={{marginLeft:"1rem"}}>Movies App</h1>
+        <h1 style={{marginLeft:"3rem"}}>Favourites</h1>
+      </div>
+    )
+  }
+}
+
